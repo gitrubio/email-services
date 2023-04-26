f1f4905cd782d393d5960a80f5a73dca4f46941a
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-04-26 11:37:42 -0500

    fix : datos

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 export const config = {
     port : process.env.server_port,
     emails : {
-        user: 'carloviloria0@gmail.com',
-        pass: 'rvijpztpfeqpxvvo',
+        user: '',
+        pass: '',
     }
 }
