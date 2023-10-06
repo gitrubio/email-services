9b223fd4699a1a57258dbb03b26ab87410726954
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-10-06 14:00:56 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,11 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended : true}))
 app.use(morgan('dev'))
+app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      next();
+    });
 //*******/
 app.use('/api',emailRoutes)
 app.listen(config.port,()=>{
