205ef603224cb863ccadfdd838d614efe3b8fc11
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-10-06 13:11:47 -0500

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,6 @@ export const config = {
     port : process.env.server_port,
     emails : {
         user: 'carloviloria0@gmail.com',
-        pass: 'rvijpztpfeqpxvvo',
+        pass: 'nppn gjgi odli fsnx',
     }
 }
